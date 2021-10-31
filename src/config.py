f5c00d48287deab700a005f007c12439c38e6259
Jakub Brojacz <kubabr@gmail.com>
2021-10-31 13:09:45 +0100

    Add ffmpeg v2

--- src/config.py
+++ src/config.py
@@ -2,5 +2,13 @@ import pathlib
 
 
 ROOT_PATH = pathlib.Path(__file__).absolute().parent.parent
+TMP_PATH = ROOT_PATH / 'tmp'
+
+
 JULIUS_PATH = ROOT_PATH / 'julius'
 JULIUS_EXE_PATH = JULIUS_PATH / 'julius-dnn.exe'
+
+
+FFMPEG_PATH = ROOT_PATH / "ffmpeg" / "ffmpeg.exe"
+FFPROBE_PATH = ROOT_PATH / "ffmpeg" / "ffprobe.exe"
+
