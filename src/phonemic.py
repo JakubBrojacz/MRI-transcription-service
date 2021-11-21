dbcb7fa4fe647e47476d1027ab6c988680e3bdb8
Jakub Brojacz <kubabr@gmail.com>
2021-11-21 21:52:34 +0100

    Add g2p

---
+++ src/phonemic.py
@@ -0,0 +1,28 @@
+import unicodedata
+
+import epitran
+import pandas as pd
+
+import config
+
+
+class G2P:
+    def __init__(self):
+        df = pd.read_csv(config.ROOT_PATH / 'aaa.tsv', sep='\t',
+                         header=None, engine='python', encoding="utf-8")
+        self.simplification_table = {
+            row[1][1]: row[1][6][0]
+            for row in df.iterrows()
+        }
+        self.epi = epitran.Epitran('pol-Latn')
+
+    def simplify(self, s):
+        for key in self.simplification_table:
+            s = s.replace(key, self.simplification_table[key])
+        s = unicodedata.normalize('NFKD', s).encode('ascii', 'ignore')
+        s = s.decode().strip()
+        return "".join(s.split())
+
+    def transliterate(self, s):
+        output_phon = self.epi.transliterate(s)
+        return self.simplify(output_phon)
