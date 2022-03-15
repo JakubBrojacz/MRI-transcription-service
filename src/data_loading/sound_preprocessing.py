464480bd3bbf0b9f868c21c254def4bff1e59d07
Jakub Brojacz <kubabr@gmail.com>
2022-03-15 12:29:53 +0100

    Simplify sound preprocessing

--- src/data_loading/sound_preprocessing.py
+++ src/data_loading/sound_preprocessing.py
@@ -14,24 +14,26 @@ AudioSegment.ffprobe = str(config.FFPROBE_PATH)
 
 
 def convert_m4a_wav(input_file: pathlib.Path, output_dir: pathlib.Path):
+    output_dir.mkdir(exist_ok=True)
     output_filename = output_dir / (input_file.stem + '.wav')
     if input_file.suffix == '.wav':
         track = AudioSegment.from_file(input_file, 'wav')
     else:
         track = AudioSegment.from_file(input_file, 'm4a')
     track = track.set_frame_rate(16000)
+    track = track.set_channels(1)
     track += 30
     track = track.low_pass_filter(1000).high_pass_filter(1000)
     track += 10
     track.export(output_filename, 'wav', bitrate="192k")
     rate, data = wavfile.read(output_filename)
-    reduced_noise = noisereduce.reduce_noise(y=data, sr=rate)
-    output_filename1 = output_dir / (input_file.stem + '1.wav')
-    wavfile.write(output_filename1, rate, reduced_noise)
-    track = AudioSegment.from_file(output_filename1, 'wav')
-    track += 15
-    output_filename2 = output_dir / (input_file.stem + '2.wav')
-    track.export(output_filename2, 'wav', bitrate="192k")
+    # reduced_noise = noisereduce.reduce_noise(y=data, sr=rate)
+    # output_filename1 = output_dir / (input_file.stem + '1.wav')
+    # wavfile.write(output_filename1, rate, reduced_noise)
+    # track = AudioSegment.from_file(output_filename1, 'wav')
+    # track += 15
+    # output_filename2 = output_dir / (input_file.stem + '2.wav')
+    # track.export(output_filename2, 'wav', bitrate="192k")
     return output_filename
 
 
